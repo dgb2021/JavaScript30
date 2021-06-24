bd662f542a2d714e68d746488200de64c4d3eb6b
Doug Barr <douglas.barr01@gmail.com>
2021-06-24 16:57:57 +0100

    added js and styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,33 @@ class PagesController < ApplicationController
   
   def index
   end
+
+  def drumkit
+  end
+
+  def clock
+  end
+
+  def variables
+  end
+
+  def cardio
+  end
+
+  def gallery
+  end
+
+  def ajax
+  end
+
+  def cardiotwo
+  end
+
+  def canvas
+  end
+
+  def tricks
+  end
   
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'index', to: 'pages#index'
+  get 'drumkit', to: 'pages#drumkit'
+  get 'clock', to: 'pages#clock'
+  get 'variables', to: 'pages#variables'
+  get 'cardio', to: 'pages#cardio'
+  get 'gallery', to: 'pages#gallery'
+  get 'ajax', to: 'pages#ajax'
+  get 'cardiotwo', to: 'pages#cardiotwo'
+  get 'canvas', to: 'pages#canvas'
+  get 'tricks', to: 'pages#tricks'
 end
