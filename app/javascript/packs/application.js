981e2d78c8625e00464c895244d7ede3643f31d4
Doug Barr <douglas.barr01@gmail.com>
2021-06-30 13:32:18 +0100

    video player

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,28 @@ require("turbolinks").start()
 require("channels")
 
 
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { togglePlay } from 'packs/scripts';
+import { updateButton } from 'packs/scripts';
+import { skip } from 'packs/scripts';
+import { handleRangeUpdate } from 'packs/scripts';
+import { handleProgress } from 'packs/scripts';
+import { scrub } from 'packs/scripts';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  togglePlay();
+  updateButton();
+  skip();
+  handleRangeUpdate();
+  handleProgress();
+  scrub();
+});
